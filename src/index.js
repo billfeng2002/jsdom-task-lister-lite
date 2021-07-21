360ce40fd6ed96ba7f5a5aa9cf60fc2d96886f1b
Bill <billfeng2002@gmail.com>
2021-07-21 14:22:00 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addNewTask(event.target[0].value)
+
+  });
+})
+let tasks=[]
+function addNewTask(name){
+  console.log(name)
+  list=document.querySelector("#tasks")
+  item=document.createElement("li")
+  item.innerHTML=name
+  tasks.push(item)
+  list.append(item)
+}
